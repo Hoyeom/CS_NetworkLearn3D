4d4219b1405e8b3795eca1071e67d3dc025b15ad
Hoyeom <korehdrb@gmail.com>
2022-04-18 23:41:49 +0900

    Add UniRx,Dotween, Player HealtBar

---
+++ Assets/Scripts/Runtime/Components/FaceCamera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Runtime.Components
+{
+    public class FaceCamera : MonoBehaviour
+    {
+        private Transform mainCamTransform;
+
+        private void Awake()
+        {
+            mainCamTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(
+                transform.position + mainCamTransform.rotation * Vector3.forward,
+                mainCamTransform.rotation * Vector3.up);
+        }
+    }
+}
--- Assets/Scripts/Runtime/Player/PlayerController.cs
+++ Assets/Scripts/Runtime/Player/PlayerController.cs
@@ -1,22 +1,33 @@
+using DG.Tweening;
 using Mirror;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.UI;
 
 namespace Runtime.Player
 {
     public class PlayerController : NetworkBehaviour,IAttackAble
     {
-        [SyncVar] private float curHealth = 100;
+        [SyncVar(hook = nameof(HookedCurHealth))] 
+        private float curHealth = 100;
+        private float maxHealth = 100;
         
         [SerializeField] private CharacterController charController;
         [SerializeField] private Transform camera = null;
-        [SerializeField] private float speed = 3;
+        
+        [SerializeField] private CanvasGroup healthGroup;
+        [SerializeField] private Image healthImage;
+        
+        private float speed = 3;
     
         private Vector3 inputVector = Vector3.zero;
         private Controller controller;
         private float xRotation;
-        
+
+        #region Client
+
         private void FixedUpdate()
         {
             if(!hasAuthority) { return; }
@@ -53,7 +64,63 @@ namespace Runtime.Player
         
             controller.Enable();
         }
+        
+        
 
+        [Command]
+        private void CmdFire(IAttackAble attackAble)
+        {
+            attackAble?.TakeDamage(5);
+        }
+        
+        private void Move(Vector3 input)
+        {
+            Vector3 move = transform.right * input.x + transform.forward * input.z;
+
+            charController.Move((move * speed + Physics.gravity) * Time.fixedDeltaTime);
+        }
+
+        #endregion
+        
+        #region Server
+
+        public NetworkIdentity GetIdentity()
+        {
+            return GetComponent<NetworkIdentity>();
+        }
+
+        
+        [Server]
+        public void TakeDamage(float damage)
+        {
+            curHealth -= damage;
+            Debug.Log($"{gameObject.name}은 {damage}피해를 입었따! 현재체력이당!: {curHealth}");
+        }
+
+        private void HookedCurHealth(float oldValue, float newValue)
+        {
+            if(hasAuthority) { return; }
+
+            healthGroup.alpha = 1;
+
+            healthImage.DOFillAmount(newValue / maxHealth, 0.3f);
+
+            healthGroup.DOFade(0, 1).Restart();
+        }
+
+        #endregion
+
+        
+        #region InputSystem
+
+        private void Canceled_Move(InputAction.CallbackContext context) => inputVector = Vector3.zero;
+
+        private void Performed_Move(InputAction.CallbackContext context)
+        {
+            Vector2 temp = context.ReadValue<Vector2>();
+            inputVector = new Vector3(temp.x, 0, temp.y);
+        }
+        
         private void Started_Fire(InputAction.CallbackContext context)
         {
             Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
@@ -69,39 +136,7 @@ namespace Runtime.Player
             }
         }
 
-        [Command]
-        private void CmdFire(IAttackAble attackAble)
-        {
-            attackAble?.TakeDamage(5);
-        }
-        
-        private void Move(Vector3 input)
-        {
-            Vector3 move = transform.right * input.x + transform.forward * input.z;
-
-            charController.Move((move * speed + Physics.gravity) * Time.fixedDeltaTime);
-        }
-
-        [Server]
-        public void TakeDamage(float damage)
-        {
-            curHealth -= damage;
-            Debug.Log($"{gameObject.name}은 {damage}피해를 입었따! 현재체력이당!: {curHealth}");
-        }
-
-        public NetworkIdentity GetIdentity()
-        {
-            return GetComponent<NetworkIdentity>();
-        }
-
-
-        private void Canceled_Move(InputAction.CallbackContext context) => inputVector = Vector3.zero;
-
-        private void Performed_Move(InputAction.CallbackContext context)
-        {
-            Vector2 temp = context.ReadValue<Vector2>();
-            inputVector = new Vector3(temp.x, 0, temp.y);
-        }
+        #endregion
 
     }
 }
